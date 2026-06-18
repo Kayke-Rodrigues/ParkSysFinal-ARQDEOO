fdac62bc514ec27166ad7a3f9bd250b40e3398bd
Kayke <kayke.rodriguesr@hotmail.com>
2026-06-18 15:51:26 -0300

    feat: adiciona VeiculoNaoEncontrado

---
+++ src/ParkSys/exceptions/VeiculoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package ParkSys.exceptions;
+
+public class VeiculoNaoEncontradoException extends Exception {
+
+    public VeiculoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
